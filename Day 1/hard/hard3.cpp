5da4d0e2ae93810a7f926995f83fd371ca45b092
Manikandan <manikandan@JokerPanda.JokerPanda>
2025-04-02 13:52:17 +0530

    added hard3 changed

--- Day 1/hard/hard3.cpp
+++ Day 1/hard/hard3.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main(){
     string u,p;
+    string cu, cp;
+    cout<<"Sign-in\nEnter correct username: "<<endl;
+    cin>>cu;
+    cout<<"Enter correct password: "<<endl;
+    cin>>cp;
     cout<<"Enter your username: ";
     cin>>u;
     cout<<"Enter your password: ";
     cin>>p;
-    string cu = "Apple", cp = "Banana";
+    
     if(u==cu && p==cp) cout<<"Logged in! Welcome "<<cu<<endl;
     else cout<<"Incorrect username or password"<<endl;
     return 0;
