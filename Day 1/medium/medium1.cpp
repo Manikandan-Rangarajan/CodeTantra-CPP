842a2c855ebb4b9df713b05dc43081f5f61b1726
Aashu1308 <aashu.sekhar@gmail.com>
2025-04-02 12:03:01 +0530

    medium added

--- Day 1/medium/medium1.cpp
+++ Day 1/medium/medium1.cpp
@@ -5,14 +5,17 @@ int main(){
     int n1,n2,n3;
     cout<<"Enter 3 numbers: ";
     cin>>n1>>n2>>n3;
-    if(n1>n2 && n1>n3){
-        cout<<n1<<" is the largest"<<endl;
+    if(n1>=n2 && n1>=n3){
+        cout<<"n1 is the largest"<<endl;
+        if (n1==n2) cout<<"n2 equals n1"<<endl;
+        if (n1==n3) cout<<"n3 equals n1"<<endl;
         }
-    else if(n2>n1 && n2>n3){
-        cout<<n2<<" is the largest"<<endl;
+    else if(n2>n1 && n2>=n3){
+        cout<<"n2 is the largest"<<endl;
+        if (n2==n3) cout<<"n3 equals n2"<<endl;
         }
     else{
-        cout<<n3<<" is the largest"<<endl;
+        cout<<"n3 is the largest"<<endl;
     }
     return 0;
 }
